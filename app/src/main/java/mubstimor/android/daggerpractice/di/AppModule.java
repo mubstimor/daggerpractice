4abaaf36f7cc907deca83dde0ae31870efc1ee31
Timothy Mubiru <mubstimor@gmail.com>
2019-08-08 13:44:19 +0300

    setup singleton instances

--- app/src/main/java/mubstimor/android/daggerpractice/di/AppModule.java
+++ app/src/main/java/mubstimor/android/daggerpractice/di/AppModule.java
@@ -9,6 +9,8 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 import mubstimor.android.daggerpractice.R;
@@ -16,6 +18,7 @@ import mubstimor.android.daggerpractice.R;
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     static RequestOptions provideRequestOptions(){
         return RequestOptions
@@ -23,12 +26,14 @@ public class AppModule {
                 .error(R.drawable.ic_launcher_background);
     }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions){
         return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions);
     }
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable(Application application){
         return ContextCompat.getDrawable(application, R.drawable.reze_logo_hd);
