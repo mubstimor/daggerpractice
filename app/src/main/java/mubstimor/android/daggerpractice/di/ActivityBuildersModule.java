d0156e33cf7d5796069271e08c082a972d85d6ec
Timothy Mubiru <mubstimor@gmail.com>
2019-08-12 16:39:14 +0300

    custom scope samples

--- app/src/main/java/mubstimor/android/daggerpractice/di/ActivityBuildersModule.java
+++ app/src/main/java/mubstimor/android/daggerpractice/di/ActivityBuildersModule.java
@@ -3,9 +3,11 @@ package mubstimor.android.daggerpractice.di;
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 import mubstimor.android.daggerpractice.di.auth.AuthModule;
+import mubstimor.android.daggerpractice.di.auth.AuthScope;
 import mubstimor.android.daggerpractice.di.auth.AuthViewModelsModule;
 import mubstimor.android.daggerpractice.di.main.MainFragmentBuildersModule;
 import mubstimor.android.daggerpractice.di.main.MainModule;
+import mubstimor.android.daggerpractice.di.main.MainScope;
 import mubstimor.android.daggerpractice.di.main.MainViewModelsModule;
 import mubstimor.android.daggerpractice.ui.auth.AuthActivity;
 import mubstimor.android.daggerpractice.ui.main.MainActivity;
@@ -13,11 +15,13 @@ import mubstimor.android.daggerpractice.ui.main.MainActivity;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {AuthViewModelsModule.class, AuthModule.class}
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainFragmentBuildersModule.class, MainViewModelsModule.class, MainModule.class}
     )
--- app/src/main/java/mubstimor/android/daggerpractice/di/main/MainModule.java
+++ app/src/main/java/mubstimor/android/daggerpractice/di/main/MainModule.java
@@ -2,6 +2,7 @@ package mubstimor.android.daggerpractice.di.main;
 
 import dagger.Module;
 import dagger.Provides;
+import mubstimor.android.daggerpractice.di.auth.AuthScope;
 import mubstimor.android.daggerpractice.network.main.MainApi;
 import mubstimor.android.daggerpractice.ui.main.posts.PostsRecyclerAdapter;
 import retrofit2.Retrofit;
@@ -9,11 +10,13 @@ import retrofit2.Retrofit;
 @Module
 public class MainModule {
 
+    @MainScope
     @Provides
     static PostsRecyclerAdapter provideAdapter(){
         return new PostsRecyclerAdapter();
     }
 
+    @MainScope
     @Provides
     static MainApi provideMainApi(Retrofit retrofit){
         return retrofit.create(MainApi.class);
